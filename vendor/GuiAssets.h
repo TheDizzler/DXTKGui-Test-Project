89293b6613617a5fa4accbded95db86f635f84ad
TheDizzler <tgillon.music@gmail.com>
2018-01-27 21:14:04 +0900

    oops damn

--- vendor/GuiAssets.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-namespace GUIAssets {
-
-	const static char* assetManifestFile = "DXTKGui/assets/DXTKGuiManifest.xml";
-	const static char* defaultFontFile = "DXTKGui/assets/Fonts/Arial.spritefont";
-};
